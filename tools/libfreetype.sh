eab48fa5343f55ecba498c2c2cd1186a812a6d22
Tommy Nguyen <tn0502@gmail.com>
2011-10-06 19:53:07 +0200

    Added/improved scripts for handling FreeType and libpng updates.

--- tools/libfreetype.sh
+++ tools/libfreetype.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-find . -name "*.mk" -o -name "Jamfile" -o -name "README" | xargs rm
-rm -r src/{autofit,bdf,cache,cid,gxvalid,gzip,lzw,otvalid,pcf,pfr,psaux,tools,type1,type42,winfonts}
-rm src/base/{ftbdf.c,ftcid.c,ftfstype.c,ftgasp.c,ftgxval.c,ftlcdfil.c,ftmac.c,ftmm.c,ftotval.c,ftpatent.c,ftpfr.c,fttype1.c,ftwinfnt.c,ftxf86.c}
+rm -r ChangeLog* Jam* Makefile README* autogen.sh builds configure devel docs modules.cfg objs version.sed vms_make.com \
+src/{autofit,bdf,cache,cid,gxvalid,gzip,lzw,otvalid,pcf,pfr,psaux,tools,type1,type42,winfonts} \
+src/base/{ftbdf.c,ftcid.c,ftfstype.c,ftgasp.c,ftgxval.c,ftlcdfil.c,ftmac.c,ftmm.c,ftotval.c,ftpatent.c,ftpfr.c,fttype1.c,ftwinfnt.c,ftxf86.c}
+patch include/freetype/config/ftmodule.h < unused.patch
 
 echo "Disable the following files:"
 echo
@@ -16,18 +17,5 @@ echo " - base/ftstream.c"
 echo " - base/fttrigon.c"
 echo " - base/ftutil.c"
 echo
-echo "Also disable any files in cff/, pshinter/, psnames/, raster/, sfnt/, smooth/, truetype/ except the ones that match <dirname>.c"
-echo
-echo "Edit include/freetype/config/ftmodule.h and comment the following modules:"
-echo
-echo " - autofit_module_class"
-echo " - t1_driver"
-echo " - t1cid_driver_class"
-echo " - pfr_driver_class"
-echo " - t42_driver_class"
-echo " - winfnt_driver_class"
-echo " - pcf_driver_class"
-echo " - psaux_module_class"
-echo " - bdf_driver_class"
-echo
-echo "Add the following C flags: -DFT2_BUILD_LIBRARY -DDARWIN_NO_CARBON"
+echo "Also disable any files in cff/, pshinter/, psnames/, raster/, sfnt/, smooth/, truetype/ that do not  match <dirname>.c"
+echo "Finally, add the following C flags: -DFT2_BUILD_LIBRARY -DDARWIN_NO_CARBON"
---
+++ tools/libpng.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+rm -r contrib/gregbook contrib/pngminim contrib/pngminus contrib/pngsuite contrib/visupng/{PngFile.*,README.txt,VisualPng.*,resource.h} example.c
