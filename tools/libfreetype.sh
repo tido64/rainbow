bf853f490a9302a15c6de5c89975d1a9045fc94c
Tommy Nguyen <tn0502@gmail.com>
2011-11-04 13:20:26 +0100

    Silenced FreeType script complaints about deleting non-existing files.

--- tools/libfreetype.sh
+++ tools/libfreetype.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rm -r ChangeLog* Jam* Makefile README* autogen.sh builds configure devel docs modules.cfg objs version.sed vms_make.com \
+rm -fr ChangeLog* Jam* Makefile README* autogen.sh builds configure devel docs modules.cfg objs version.sed vms_make.com \
 src/{autofit,bdf,cache,cid,gxvalid,gzip,lzw,otvalid,pcf,pfr,psaux,tools,type1,type42,winfonts} \
 src/base/{ftbdf.c,ftcid.c,ftfstype.c,ftgasp.c,ftgxval.c,ftlcdfil.c,ftmac.c,ftmm.c,ftotval.c,ftpatent.c,ftpfr.c,fttype1.c,ftwinfnt.c,ftxf86.c}
 patch include/freetype/config/ftmodule.h < unused.patch
