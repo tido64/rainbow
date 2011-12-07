31bfa86e308c46a8ab6ce869628297258545e207
Tommy Nguyen <tn0502@gmail.com>
2011-12-07 13:06:52 +0100

    Label class is set to replace printing functions in FontAtlas.

---
+++ src/Graphics/Label.cpp
@@ -0,0 +1,104 @@
+/// Copyright 2011 Bifrost Games. All rights reserved.
+/// \author Tommy Nguyen
+
+#include "Graphics/Label.h"
+
+Label::Label(const char *s) :
+	length(0), size(0), text(nullptr), vx(nullptr)
+{
+	this->set_text(s);
+}
+
+Label::~Label()
+{
+	delete[] this->text;
+	delete[] this->vx;
+}
+
+void Label::draw()
+{
+	glDisableClientState(GL_COLOR_ARRAY);
+	glColor4ub(this->color.r, this->color.g, this->color.b, this->color.a);
+	this->font->bind();
+
+	if (this->vx)
+	{
+		glTexCoordPointer(2, GL_FLOAT, sizeof(SpriteVertex), &this->vx->texcoord);
+		glVertexPointer(2, GL_FLOAT, sizeof(SpriteVertex), &this->vx->position);
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, this->length << 2);
+	}
+	else
+	{
+		glPushMatrix();
+		glTranslatef(static_cast<GLfloat>(this->position.x), static_cast<GLfloat>(this->position.y), 0.0f);
+
+		#ifdef FONTATLAS_KERNING
+			unsigned int prev = 0;  // 'space' has no kerning
+		#endif
+		for (const char *text = this->text; *text; ++text)
+		{
+			const FontGlyph &glyph = this->font->get_glyph(*text);
+
+			#ifdef FONTATLAS_KERNING
+				glTranslatef(static_cast<float>(glyph.left + glyph.kern[prev]), 0.0f, 0.0f);
+				prev = static_cast<int>(*text) - FontAtlas::ascii_offset;
+			#else
+				glTranslatef(static_cast<float>(glyph.left), 0.0f, 0.0f);
+			#endif
+			glTexCoordPointer(2, GL_FLOAT, sizeof(SpriteVertex), &glyph.quad[0].texcoord);
+			glVertexPointer(2, GL_FLOAT, sizeof(SpriteVertex), &glyph.quad[0].position);
+			glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+			glTranslatef(static_cast<float>(glyph.advance - glyph.left), 0.0f, 0.0f);
+		}
+
+		glPopMatrix();
+	}
+
+	glColor4ub(0xff, 0xff, 0xff, 0xff);
+	glEnableClientState(GL_COLOR_ARRAY);
+}
+
+void Label::finalize()
+{
+	// Note #1: This algorithm does not create degenerate vertices.
+	// Note #2: This algorithm currently does not support font kerning.
+
+	const unsigned int str_sz = this->length << 2;
+	if (str_sz > this->size)
+	{
+		delete[] this->vx;
+		this->vx = new SpriteVertex[str_sz];
+		this->size = str_sz;
+	}
+
+	SpriteVertex *vx = this->vx;
+	Vec2f pen = this->position;
+	for (const char *text = this->text; *text; ++text)
+	{
+		const FontGlyph &glyph = this->font->get_glyph(*text);
+		pen.x += glyph.left;
+
+		for (unsigned int i = 0; i < 4; ++i)
+		{
+			vx->texcoord = glyph.quad[i].texcoord;
+			vx->position = glyph.quad[i].position;
+			vx->position += pen;
+			++vx;
+		}
+
+		pen.x += glyph.advance - glyph.left;
+	}
+}
+
+void Label::set_text(const char *text)
+{
+	const unsigned int len = strlen(text);
+	if (len > this->length)
+	{
+		delete[] this->text;
+		this->text = new char[len + 1];
+		this->length = len;
+	}
+	memcpy(this->text, text, len);
+	this->text[len] = '\0';
+}
---
+++ src/Graphics/Label.h
@@ -0,0 +1,77 @@
+#ifndef LABEL_H_
+#define LABEL_H_
+
+#include <cstring>
+
+#include "Common/SmartPtr.h"
+#include "Graphics/Drawable.h"
+#include "Graphics/FontAtlas.h"
+
+/// Label for displaying text.
+///
+/// \note Current algorithm is not optimized in any way. Text is rendered
+///       character by character (ie. a glDraw per character) without any form
+///       of caching.
+///
+/// Copyright 2011 Bifrost Games. All rights reserved.
+/// \author Tommy Nguyen
+class Label : public Drawable
+{
+public:
+	Label();
+	Label(const char *);
+	~Label();
+
+	/// Draw text at previously set position.
+	virtual void draw();
+
+	/// Render string to a bitmap.
+	void finalize();
+
+	/// Set text color.
+	void set_color(const Colorb &);
+
+	/// Set text font.
+	void set_font(FontAtlas *);
+
+	/// Set position of text.
+	void set_position(const int x, const int y);
+
+	/// Set text to display.
+	void set_text(const char *);
+
+	/// Does absolutely nothing (for now).
+	virtual void update();
+
+private:
+	unsigned int length;       ///< Length of the string.
+	unsigned int size;         ///< Size of the vertex array.
+	char *text;                ///< Content of this label.
+	SpriteVertex *vx;          ///< Vertex array containing the text.
+	SmartPtr<FontAtlas> font;  ///< The font used in this label.
+	Vec2f position;            ///< Position of the text (top left).
+	Colorb color;              ///< Color of the text.
+};
+
+inline Label::Label() :
+	length(0), size(0), text(nullptr), vx(nullptr) { }
+
+inline void Label::set_color(const Colorb &c)
+{
+	this->color = c;
+}
+
+inline void Label::set_font(FontAtlas *f)
+{
+	this->font = f;
+}
+
+inline void Label::set_position(const int x, const int y)
+{
+	this->position.x = x;
+	this->position.y = y;
+}
+
+inline void Label::update() { }
+
+#endif
