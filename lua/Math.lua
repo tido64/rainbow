135a5e637470fa4aacbc6966a3d78ec3b58c2e8d
Tommy Nguyen <tn0502@gmail.com>
2012-02-17 15:04:09 +0100

    Added helper function to check whether a point is inside a box.

--- lua/Math.lua
+++ lua/Math.lua
@@ -1,5 +1,7 @@
 --! Rainbow math helper.
 --!
+--! Because some of us don't have a PhD in math.
+--!
 --! Note that if you're going to use any of these in a loop, it is better to
 --! inline them, e.g.:
 --!
@@ -33,12 +35,24 @@ function rainbow.math.angle(a_x, a_y, b_x, b_y)
 	return math.atan((b_y - a_y) / (b_x - a_x));
 end
 
---! Convert degrees to radians.
-function rainbow.math.deg2rad(degrees)
-	return degrees * math.pi / 180;
+--! Convert radians to degrees.
+function rainbow.math.degrees(radians)
+	return radians * 180 / math.pi;
+end
+
+--! Calculate the distance between two points.
+--! \param a_x,a_y  Starting point.
+--! \param b_x,b_y  End point.
+function rainbow.math.distance(a_x, a_y, b_x, b_y)
+	local s1 = b_x - a_x;
+	local s2 = b_y - a_y;
+	return math.sqrt(s1 * s1, s2 * s2);
 end
 
 --! Calculate the hitbox.
+--! \param x,y           Centre of the box.
+--! \param width,height  Dimension of the box.
+--! \param scale         Scaling factor for on-screen box size.
 function rainbow.math.hitbox(x, y, width, height, scale)
 	if not scale then
 		scale = 1.0;
@@ -53,16 +67,14 @@ function rainbow.math.hitbox(x, y, width, height, scale)
 	return hitbox;
 end
 
---! Calculate the distance between two points.
---! \param a_x,a_y  Starting point.
---! \param b_x,b_y  End point.
-function rainbow.math.distance(a_x, a_y, b_x, b_y)
-	local s1 = b_x - a_x;
-	local s2 = b_y - a_y;
-	return math.sqrt(s1 * s1, s2 * s2);
+--! Check whether a point is inside a box.
+--! \param box    Table with the upper-left and lower-right points of the box.
+--! \param point  The point to check.
+function rainbow.math.is_inside(box, point)
+	return point.x >= box.x0 && point.x <= box.x1 && point.y >= box.y0 && point.y <= box.y1;
 end
 
---! Convert radians to degrees.
-function rainbow.math.rad2deg(radians)
-	return radians * 180 / math.pi;
+--! Convert degrees to radians.
+function rainbow.math.radians(degrees)
+	return degrees * math.pi / 180;
 end
