266686f123d3bd7d33d10470b264fcbca5518de0
Tommy Nguyen <tn0502@gmail.com>
2012-03-22 00:24:38 +0100

    Reimplemented Scheduler to use a more precise ticker.

--- lua/Scheduler.lua
+++ lua/Scheduler.lua
@@ -1,5 +1,5 @@
 --! Scheduler class makes sure objects are notifed at specified intervals.
-
+--!
 --! All objects must implement the following function:
 --!
 --! - tick()
@@ -7,62 +7,63 @@
 --! Copyright 2011-12 Bifrost Entertainment. All rights reserved.
 --! \author Tommy Nguyen
 
-Scheduler = {};
-Scheduler.__index = Scheduler;
+Scheduler = {}
+Scheduler.__index = Scheduler
 
-function Scheduler.new(secs)
-	local a = {};
-	setmetatable(a, Scheduler);
-	a.count = 0;
-	a.subscribers = {};
-	a.ticker = "0";
-	a.time = 0;
-	a.timeout = secs;
-	return a;
+--! Create a scheduler that ticks every \p t seconds or milliseconds. If \p t
+--! is less than 300, it is interpreted as seconds. Otherwise, it's
+--! milliseconds.
+function Scheduler.new(t)
+	if t < 300 then
+		t = t * 1000
+	end
+	local self = {}
+	setmetatable(self, Scheduler)
+	self.count = 0
+	self.elapsed = 0
+	self.subscribers = {}
+	self.timeout = t
+	return self
 end
 
 --! Add an object that needs to be notified.
 function Scheduler:add(obj)
-	self.count = self.count + 1;
-	self.subscribers[self.count] = obj;
+	self.count = self.count + 1
+	self.subscribers[self.count] = obj
 end
 
 --! Remove object from scheduler.
 function Scheduler:remove(obj)
-	local sub = 0;
 	for i = 1, self.count do
 		if self.subscribers[i] == obj then
-			sub = i;
-			break;
+			self.subscribers[i] = self.subscribers[self.count]
+			self.subscribers[self.count] = nil
+			self.count = self.count - 1
+			break
 		end
 	end
-	if sub == 0 then
-		return;
-	end
-	self.subscribers[sub] = self.subscribers[self.count];
-	self.subscribers[self.count] = nil;
-	self.count = self.count - 1;
 end
 
 --! Remove all objects from scheduler.
 function Scheduler:remove_all()
 	for i = 1, self.count do
-		self.subscribers[i] = nil;
+		self.subscribers[i] = nil
 	end
-	self.count = 0;
+	self.count = 0
+	self.elapsed = 0
 end
 
 --! Notify all objects at specified interval.
-function Scheduler:update()
-	local time = os.date("%Y%m%d%H%M%S");
-	if time > self.ticker then
-		self.ticker = time;
-		self.time = self.time + 1;
-		if self.time > self.timeout then
-			for i = 1, self.count do
-				self.subscribers[i]:tick();
-			end
-			self.time = 1;
+function Scheduler:update(dt)
+	if self.count == 0 then
+		return
+	end
+
+	self.elapsed = self.elapsed + dt
+	while self.elapsed >= self.timeout do
+		for i = self.count, 1, -1 do
+			self.subscribers[i]:tick()
 		end
+		self.elapsed = self.elapsed - self.timeout
 	end
 end
