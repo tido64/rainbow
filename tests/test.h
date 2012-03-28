e4adb7c36b77d3d795b0c9e11f3815d47f737685
Tommy Nguyen <tn0502@gmail.com>
2012-03-28 22:58:44 +0200

    Fixed some tests after recent API changes.

--- tests/test.h
+++ tests/test.h
@@ -2,11 +2,10 @@
 
 #include "Common/Chrono.test"
 #include "Common/Color.test"
-#include "Common/Data.test"
+//#include "Common/Data.test"
 #include "Common/List.test"
 #include "Common/Vec2.test"
 #include "Common/Vec3.test"
 #include "Common/Vector.test"
-#include "Input/Acceleration.test"
 #include "Input/Touch.test"
 #include "Algorithm.test"
