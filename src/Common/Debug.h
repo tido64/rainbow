bf68e1ac620e57673c2fdebcb70c79e3625b194e
Tommy Nguyen <tn0502@gmail.com>
2012-08-19 22:44:50 +0200

    Implemented additional debugging functions.

--- src/Common/Debug.h
+++ src/Common/Debug.h
@@ -1,26 +1,22 @@
 #ifndef RAINBOW_DEBUG_H_
 #define RAINBOW_DEBUG_H_
 
-namespace Rainbow
-{
-	template<bool>
-	struct _static_assert;
+#include <cstdio>
 
-	/// Static assert is inspired by Boost.StaticAssert and aims to implement
-	/// simple compile-time assertion.
-	///
-	/// Copyright 2011-12 Bifrost Entertainment. All rights reserved.
-	/// \author Tommy Nguyen
-	template<>
-	struct _static_assert<true> { };
-}
+#include "Platform.h"
+#ifdef RAINBOW_ANDROID
+#	include <android/log.h>
+#	define R_ERROR(...) __android_log_print(ANDROID_LOG_ERROR, "Rainbow", __VA_ARGS__)
+#else
+#	define R_ERROR(...) fprintf(stderr, __VA_ARGS__)
+#endif
 
 #ifndef NDEBUG
-#	ifndef __GNUC__
-#		define __PRETTY_FUNCTION__ __FUNCTION__
-#	endif
 
-#include <cstdio>
+#ifndef __GNUC__
+#	define __PRETTY_FUNCTION__ __FUNCTION__
+#endif
+
 #include <cstdlib>
 
 #define LUA_ASSERT(expr, syntax) \
@@ -28,13 +24,13 @@ namespace Rainbow
 	{ \
 		if (!(expr)) \
 		{ \
-			fprintf(stderr, "[Rainbow] Lua syntax: %s\n", syntax); \
+			R_ERROR("[Rainbow] Lua syntax: %s\n", syntax); \
 			unsigned int depth = 0; \
 			lua_Debug entry; \
 			while (lua_getstack(L, depth, &entry)) \
 			{ \
 				lua_getinfo(L, "nSl", &entry); \
-				fprintf(stderr, "%s:%d: %s\n", entry.short_src, entry.currentline, entry.name ? entry.name : ""); \
+				R_ERROR("%s:%d: %s\n", entry.short_src, entry.currentline, entry.name ? entry.name : ""); \
 				++depth; \
 			} \
 			abort(); \
@@ -50,13 +46,34 @@ namespace Rainbow
 	{ \
 		if (!(expr)) \
 		{ \
-			fprintf(stderr, "[Rainbow] %s: %s (aborted at %s:%i: %s)\n", __PRETTY_FUNCTION__, reason, __FILE__, __LINE__, #expr); \
+			R_ERROR("[Rainbow] %s: %s (aborted at %s:%i: %s)\n", __PRETTY_FUNCTION__, reason, __FILE__, __LINE__, #expr); \
 			abort(); \
 		} \
 	} while (0)
 
+#ifdef RAINBOW_ANDROID
+#	define R_DEBUG(...) __android_log_print(ANDROID_LOG_DEBUG, "Rainbow", __VA_ARGS__)
+#else
+#	define R_DEBUG(...) do { fprintf(stdout, __VA_ARGS__); fflush(stdout); } while (0)
+#endif
+
 #ifndef __GXX_EXPERIMENTAL_CXX0X__
-#	define static_assert(expr, reason) Rainbow::_static_assert<(expr)>()
+
+namespace Rainbow
+{
+	template<bool>
+	struct _static_assert;
+
+	/// Static assert is inspired by Boost.StaticAssert and aims to implement
+	/// simple compile-time assertion.
+	///
+	/// Copyright 2011-12 Bifrost Entertainment. All rights reserved.
+	/// \author Tommy Nguyen
+	template<>
+	struct _static_assert<true> { };
+}
+
+#define static_assert(expr, reason) Rainbow::_static_assert<(expr)>()
 #endif
 
 #else
@@ -64,6 +81,7 @@ namespace Rainbow
 #define LUA_ASSERT(expr, syntax)   static_cast<void>(0)
 #define LUA_CHECK(L, expr, reason) static_cast<void>(0)
 #define R_ASSERT(expr, reason)     static_cast<void>(0)
+#define R_DEBUG(...)               static_cast<void>(0)
 
 #ifndef __GXX_EXPERIMENTAL_CXX0X__
 #	define static_assert(expr, reason) static_cast<void>(0)
