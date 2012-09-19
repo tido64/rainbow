5e04624b43001e4ff40f7cebf14225e97cb58785
Tommy Nguyen <tn0502@gmail.com>
2012-09-19 22:38:52 +0200

    Deprecated custom nullptr_t.

--- src/Platform.h
+++ src/Platform.h
@@ -19,16 +19,17 @@
 #	elif !defined(WINVER)
 #		define WINVER 0x0501
 #	endif
+#	define VC_EXTRALEAN
 #endif
 
 // Platforms that make use of physical buttons (keyboards, gamepads, etc.)
-// and of SDL
+// and of SDL.
 #if defined(RAINBOW_UNIX) || defined(RAINBOW_WIN)
 #	define RAINBOW_BUTTONS
 #	define RAINBOW_SDL
 #endif
 
-// Platforms with accelerometer and touch screen
+// Platforms with accelerometer and touch screen.
 #if defined(RAINBOW_ANDROID) || defined(RAINBOW_IOS)
 #	define RAINBOW_ACCELERATED 1
 #	define RAINBOW_TOUCHED 1
@@ -39,38 +40,12 @@
 
 #define RAINBOW_BUILD "Rainbow / Bifrost Entertainment Property / Built " __DATE__
 
-// Retrieve GCC compiler version
+// Define keyword override for pre-C++11 compilers.
 #ifdef __GNUC__
 #	ifndef __GNUC_PATCHLEVEL__
 #		define __GNUC_VERSION__ (__GNUC__ * 10000 + __GNUC_MINOR__ * 100)
 #	else
 #		define __GNUC_VERSION__ (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
-#	endif
-#else
-#	define __GNUC_VERSION__ 0
-#endif
-
-// Define nullptr for non-C++11-compliant compilers
-#if __MINGW32__
-#	define nullptr 0
-#	define override
-#elif __cplusplus <= 19971
-#	if __GNUC_VERSION__ < 40600
-
-const
-class nullptr_t
-{
-public:
-	template<class T>
-	inline operator T*() const { return 0; }
-
-	template<class C, class T>
-	inline operator T C::*() const { return 0; }
-
-private:
-	void operator&() const;
-} nullptr = {};
-
 #	endif
 #	if __GNUC_VERSION__ < 40700
 #		define override
