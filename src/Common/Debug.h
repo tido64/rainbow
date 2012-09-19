c2fdb2db870d86cc2edab091dfa4afaa51dbabb7
Tommy Nguyen <tn0502@gmail.com>
2012-09-19 21:53:48 +0200

    Deprecated custom static_assert().

--- src/Common/Debug.h
+++ src/Common/Debug.h
@@ -57,35 +57,12 @@
 #	define R_DEBUG(...) do { fprintf(stdout, __VA_ARGS__); fflush(stdout); } while (0)
 #endif
 
-#ifndef __GXX_EXPERIMENTAL_CXX0X__
-
-namespace Rainbow
-{
-	template<bool>
-	struct _static_assert;
-
-	/// Static assert is inspired by Boost.StaticAssert and aims to implement
-	/// simple compile-time assertion.
-	///
-	/// Copyright 2011-12 Bifrost Entertainment. All rights reserved.
-	/// \author Tommy Nguyen
-	template<>
-	struct _static_assert<true> { };
-}
-
-#define static_assert(expr, reason) Rainbow::_static_assert<(expr)>()
-#endif
-
 #else
 
 #define LUA_ASSERT(expr, syntax)   static_cast<void>(0)
 #define LUA_CHECK(L, expr, reason) static_cast<void>(0)
 #define R_ASSERT(expr, reason)     static_cast<void>(0)
 #define R_DEBUG(...)               static_cast<void>(0)
-
-#ifndef __GXX_EXPERIMENTAL_CXX0X__
-#	define static_assert(expr, reason) static_cast<void>(0)
-#endif
 
 #endif  // !NDEBUG
 #endif  // RAINBOW_DEBUG_H_
