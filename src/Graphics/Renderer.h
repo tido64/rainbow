68906d23723bb89cc5aad241546c297ad4dec322
Tommy Nguyen <tn0502@gmail.com>
2012-12-19 13:46:36 +0100

    Put Renderer functions in a namespace.

--- src/Graphics/Renderer.h
+++ src/Graphics/Renderer.h
@@ -3,31 +3,22 @@
 
 struct SpriteVertex;
 
-class Renderer
+namespace Renderer
 {
-public:
-	static bool init();
-	static void release();
+	bool init();
+	void release();
 
-	static void clear();
-	static void resize(const unsigned int width, const unsigned int height);
+	void clear();
+	void resize(const unsigned int width, const unsigned int height);
 
-	static void bind_buffer(const unsigned int buffer);
+	void bind_buffer(const unsigned int buffer);
+	void create_buffer(unsigned int &buffer, unsigned int &array_object);
+	void delete_buffer(const unsigned int buffer, const unsigned int array_object);
 
-	static void create_buffer(unsigned int &buffer, unsigned int &array_object);
-	static void delete_buffer(const unsigned int buffer, const unsigned int array_object);
+	void draw_buffer(const unsigned int array_object, const unsigned int count);
+	void draw_elements(const SpriteVertex *vertices, const unsigned int count);
 
-	static void draw_buffer(const unsigned int array_object, const unsigned int count);
-	static void draw_elements(const SpriteVertex *vertices, const unsigned int count);
-
-	static void update_buffer(const unsigned int buffer, const unsigned int size, const void *data);
-
-private:
-	static unsigned int bound_vbo;     ///< Currently bound vertex buffer object.
-	static unsigned int index_buffer;  ///< Index buffer object.
-
-	/// Global vertex indices (currently limited to 384 vertices, or 64 sprites).
-	static const unsigned short default_indices[];
-};
+	void update_buffer(const unsigned int buffer, const unsigned int size, const void *data);
+}
 
 #endif
