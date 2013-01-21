e11f1c605efcb9f792202c87023506371a22ed80
Tommy Nguyen <tn0502@gmail.com>
2013-01-21 22:45:14 +0100

    Miscellaneous.
    - Cleaned up .hgignore
    - Cleaned up build/.
    - Cleaned up CMakeLists.txt.

--- build/android/jni/Application.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-
-EXT_LIB := $(LOCAL_PATH)/../../../../lib
-
-APP_CFLAGS  := -g -Wall -I $(LOCAL_PATH)/../../../libs -I $(LOCAL_PATH)/../../../libs/freetype/include -I $(EXT_LIB) -I $(EXT_LIB)/libpng -I $(EXT_LIB)/libzip -ftree-vectorize
-APP_MODULES := onwire rainbow box2d freetype png zip
-APP_OPTIM   := debug
