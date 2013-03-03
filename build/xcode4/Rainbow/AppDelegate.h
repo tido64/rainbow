635e9140c08aaf591014f5820a93ddef6a91f6e4
Tommy Nguyen <tn0502@gmail.com>
2013-03-03 15:51:20 +0100

    iOS: Added Xcode project.

---
+++ build/xcode4/Rainbow/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Rainbow
+//
+//  Created by Tommy Nguyen on 4/4/12.
+//  Copyright (c) 2012 Bifrost Entertainment AS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RainbowViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) RainbowViewController *viewController;
+
+@end
---
+++ build/xcode4/Rainbow/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Rainbow
+//
+//  Created by Tommy Nguyen on 4/4/12.
+//  Copyright (c) 2012 Bifrost Entertainment AS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
