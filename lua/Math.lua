77160d182a3bb97f929b8fd37518d757100a7fe7
Tommy Nguyen <tn0502@gmail.com>
2013-09-20 16:07:36 +0200

    Modularized Math.lua.

--- lua/Math.lua
+++ lua/Math.lua
@@ -6,18 +6,18 @@
 --! inline them, e.g.:
 --!
 --! \code
---! -- This is BAD!
+--! local Math = require(module_path .. "Math")
+--!
 --! function bad_code()
 --! 	local d = 1.0
 --! 	while true do
---! 		d = rainbow.math.radians(d)
+--! 		d = Math.radians(d)  -- This is BAD!
 --! 	end
 --! end
 --!
---! -- This is GOOD!
 --! function good_code()
 --! 	local d = 1.0
---! 	local radians = rainbow.math.radians
+--! 	local radians = Math.radians  -- This is GOOD!
 --! 	while true do
 --! 		d = radians(d)
 --! 	end
@@ -29,7 +29,13 @@
 --! Copyright 2012-13 Bifrost Entertainment. All rights reserved.
 --! \author Tommy Nguyen
 
-rainbow.math = rainbow.math or {}
+local atan2   = math.atan2
+local max     = math.max
+local min     = math.min
+local sqrt    = math.sqrt
+
+local kDegree = math.pi / 180
+local kRadian = 180 / math.pi
 
 -- The universal gravitation constant, defined as G = 6.67384 * 10^-11 N(m/kg)^2.
 local G = 6.67384e-11
@@ -37,41 +43,33 @@ local G = 6.67384e-11
 -- Standard gravitational acceleration, defined in m/s^2.
 local g = 9.80665
 
-local kRadian = 180 / math.pi
-local kDegree = math.pi / 180
-local abs     = math.abs
-local atan2   = math.atan2
-local max     = math.max
-local min     = math.min
-local sqrt    = math.sqrt
-
 --! Calculate the angle between two points.
-function rainbow.math.angle(a_x, a_y, b_x, b_y)
-	return atan2(b_y - a_y, b_x - a_x)
+local function angle(ax, ay, bx, by)
+	return atan2(by - ay, bx - ax)
 end
 
 --! Clamp value between a range.
-function rainbow.math.clamp(x, min_val, max_val)
+local function clamp(x, min_val, max_val)
 	return min(max(x, min_val), max_val)
 end
 
 --! Convert radians to degrees.
-function rainbow.math.degrees(radians)
+local function degrees(radians)
 	return radians * kRadian
 end
 
 --! Calculate the distance between two points.
---! \param a_x,a_y  Starting point.
---! \param b_x,b_y  End point.
-function rainbow.math.distance(a_x, a_y, b_x, b_y)
-	local s1 = b_x - a_x
-	local s2 = b_y - a_y
+--! \param ax,ay  Starting point.
+--! \param bx,by  End point.
+local function distance(ax, ay, bx, by)
+	local s1 = bx - ax
+	local s2 = by - ay
 	return sqrt(s1 * s1 + s2 * s2)
 end
 
 --! Newton's law of universal gravitation:
 --! F = G * (m1 * m2) / r^2
-function rainbow.math.gravitation(m1, m2, r)
+local function gravitation(m1, m2, r)
 	return G * m1 * m2 / (r * r)
 end
 
@@ -79,7 +77,7 @@ end
 --! \param x,y           Centre of the box.
 --! \param width,height  Dimension of the box.
 --! \param scale         Scaling factor for on-screen box size.
-function rainbow.math.hitbox(x, y, width, height, scale)
+local function hitbox(x, y, width, height, scale)
 	scale = scale or 1.0
 	local half_w = width * scale * 0.5
 	local half_h = height * scale * 0.5
@@ -94,11 +92,24 @@ end
 --! Check whether a point is inside a box.
 --! \param box    Table with the upper-left and lower-right points of the box.
 --! \param point  The point to check.
-function rainbow.math.is_inside(box, point)
+local function is_inside(box, point)
 	return point.x >= box.x0 and point.x <= box.x1 and point.y >= box.y0 and point.y <= box.y1
 end
 
 --! Convert degrees to radians.
-function rainbow.math.radians(degrees)
+local function radians(degrees)
 	return degrees * kDegree
 end
+
+return {
+	G = G,
+	angle = angle,
+	clamp = clamp,
+	degrees = degrees,
+	distance = distance,
+	g = g,
+	gravitation = gravitation,
+	hitbox = hitbox,
+	is_inside = is_inside,
+	radians = radians
+}
