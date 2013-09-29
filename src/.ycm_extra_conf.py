b88a7049b997dc33bd9efebc6493ff30818d878b
Tommy Nguyen <tn0502@gmail.com>
2013-09-29 14:28:16 +0200

    Vim: Added support for YouCompleteMe.

---
+++ src/.ycm_extra_conf.py
@@ -0,0 +1,26 @@
+import os
+
+project_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
+cxx_flags = [
+	"-std=c++11",
+	"-Werror",
+	"-pedantic",
+	"-Wall",
+	"-Wextra",
+	"-Wno-invalid-offsetof",
+	"-Weffc++",
+	"-Wold-style-cast",
+	"-Woverloaded-virtual",
+	"-Wsign-promo",
+	"-I" + os.path.join(project_path, "src"),
+	"-I" + os.path.join(project_path, "lib"),
+	"-I" + os.path.join(project_path, "lib", "FreeType", "include"),
+	"-I" + os.path.join(project_path, "lib", "Lua"),
+	"-I" + os.path.join(project_path, "lib", "SDL", "include"),
+	"-I" + os.path.join(project_path, "lib", "libpng"),
+	"-fno-rtti",
+	"-fno-exceptions",
+]
+
+def FlagsForFile(file):
+	return { 'flags': cxx_flags, 'do_cache': True }
