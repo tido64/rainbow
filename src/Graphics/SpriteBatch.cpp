tido64/rainbow

commit 81778f174dd403bb59c75ccd59f89a5ad9c569a2

SpriteBatch shouldn't directly allocate new Sprites on the heap.

src/Graphics/SpriteBatch.cpp
@@ -8,20 +8,14 @@ const char Drawable::class_name[] = "Drawable";
 SpriteBatch::SpriteBatch(const size_t hint) :
 	sprites(hint), vertices(hint * 4) { }
 
-SpriteBatch::~SpriteBatch()
+Sprite& SpriteBatch::add(const int x, const int y, const int w, const int h)
 {
-	for (auto sprite : this->sprites)
-		delete sprite;
+	Sprite &sprite = this->create_sprite(w, h);
+	sprite.set_texture(this->texture->define(Vec2i(x, y), w, h));
+	return sprite;
 }
 
-Sprite* SpriteBatch::add(const int x, const int y, const int w, const int h)
-{
-	Sprite *s = this->create_sprite(w, h);
-	s->set_texture(this->texture->define(Vec2i(x, y), w, h));
-	return s;
-}
-
-Sprite* SpriteBatch::create_sprite(const unsigned int width, const unsigned int height)
+Sprite& SpriteBatch::create_sprite(const unsigned int width, const unsigned int height)
 {
 	R_ASSERT(this->sprites.size() <= 256, "Hard-coded limit reached");
 
@@ -31,8 +25,7 @@ Sprite* SpriteBatch::create_sprite(const unsigned int width, const unsigned int 
 	this->vertices.push_back(SpriteVertex());
 	this->vertices.push_back(SpriteVertex());
 
-	Sprite *s = new Sprite(width, height, this);
-	this->sprites.push_back(s);
+	this->sprites.push_back(Sprite(width, height, this));
 	this->array.count += 6;
 
 	R_ASSERT(this->sprites.size() * 4 == this->vertices.size(),
@@ -40,13 +33,16 @@ Sprite* SpriteBatch::create_sprite(const unsigned int width, const unsigned int 
 
 	// If the vertex buffer was resized, we'll need to reassign buffers.
 	if (this->vertices.capacity() != current_capacity)
+	{
 		for (size_t i = 0; i < this->sprites.size(); ++i)
-			this->sprites[i]->vertex_array = &this->vertices[i * 4];
-	else
-		// Assign the batch's buffer to the sprite.
-		s->vertex_array = &this->vertices[this->vertices.size() - 4];
+			this->sprites[i].vertex_array = &this->vertices[i * 4];
+		return this->sprites.back();
+	}
 
-	return s;
+	// Assign the batch's buffer to the sprite.
+	Sprite &sprite = this->sprites.back();
+	sprite.vertex_array = &this->vertices[this->vertices.size() - 4];
+	return sprite;
 }
 
 void SpriteBatch::draw()
@@ -69,8 +65,8 @@ TextureAtlas* SpriteBatch::set_texture(TextureAtlas *t)
 void SpriteBatch::update()
 {
 	// Update all sprite vertices
-	for (auto sprite : this->sprites)
-		sprite->update();
+	for (auto &sprite : this->sprites)
+		sprite.update();
 
 	// Update vertex buffer
 	this->array.update(this->vertices.begin(), this->vertices.size() * sizeof(SpriteVertex));

src/Graphics/SpriteBatch.h
@@ -26,20 +26,19 @@ public:
 	/// \param hint  If you know in advance how many sprites you'll need, set
 	///              \p hint for more efficient storage.
 	SpriteBatch(const size_t hint = 8);
-	~SpriteBatch();
 
 	/// Add a textured sprite to the batch given texture coordinates.
 	/// \param x,y     Position of the texture assigned to the sprite.
 	/// \param width   Width of the texture and, consequently, the sprite.
 	/// \param height  Height of the texture and, consequently, the sprite.
 	/// \return The newly created sprite, at position (0,0).
-	Sprite* add(const int x, const int y, const int width, const int height);
+	Sprite& add(const int x, const int y, const int width, const int height);
 
 	/// Create a sprite.
 	/// \param width   Width of the sprite.
 	/// \param height  Height of the sprite.
 	/// \return The newly created sprite, at position (0,0).
-	Sprite* create_sprite(const unsigned int width, const unsigned int height);
+	Sprite& create_sprite(const unsigned int width, const unsigned int height);
 
 	/// Load texture data.
 	TextureAtlas* set_texture(const DataMap &texture);
@@ -57,7 +56,7 @@ private:
 	Renderer::VertexArray array;
 
 	SharedPtr<TextureAtlas> texture;  ///< Texture atlas used by all sprites in the batch.
-	Vector<Sprite*> sprites;          ///< Vector storing all sprites.
+	Vector<Sprite> sprites;           ///< Vector storing all sprites.
 	Vector<SpriteVertex> vertices;    ///< Common vertex array for all sprites in the batch.
 };
 

src/Lua/lua_SpriteBatch.cpp
@@ -33,7 +33,7 @@ namespace Rainbow
 			const int y = luaR_tointeger(L, 2);
 			const int w = luaR_tointeger(L, 3);
 			const int h = luaR_tointeger(L, 4);
-			lua_pushlightuserdata(L, ::SpriteBatch::add(x, y, w, h));
+			lua_pushlightuserdata(L, &::SpriteBatch::add(x, y, w, h));
 			return alloc<Sprite>(L);
 		}
 
@@ -43,7 +43,7 @@ namespace Rainbow
 
 			const int w = luaR_tointeger(L, 1);
 			const int h = luaR_tointeger(L, 2);
-			lua_pushlightuserdata(L, ::SpriteBatch::create_sprite(w, h));
+			lua_pushlightuserdata(L, &::SpriteBatch::create_sprite(w, h));
 			return alloc<Sprite>(L);
 		}
 

src/Graphics/SceneGraph.cpp
@@ -47,8 +47,8 @@ namespace SceneGraph
 				this->label->move(delta);
 				break;
 			case SpriteBatchNode: {
-				for (auto sprite : this->sprite_batch->sprites)
-					sprite->move(delta);
+				for (auto &sprite : this->sprite_batch->sprites)
+					sprite.move(delta);
 				break;
 			}
 			default:

src/Graphics/Sprite.cpp
@@ -26,10 +26,14 @@ Sprite::Sprite(const unsigned int w, const unsigned int h, const SpriteBatch *p)
 	width(w), height(h), angle(0.0f), stale(-1), vertex_array(nullptr),
 	parent(p), pivot(0.5f, 0.5f), scale(1.0f, 1.0f) { }
 
-Sprite::Sprite(const Sprite &s, const SpriteBatch *p) :
-	width(s.width), height(s.height), angle(s.angle), stale(-1),
-	vertex_array(nullptr), parent(p), center(s.center), pivot(s.pivot),
-	position(s.position), scale(s.scale) { }
+Sprite::Sprite(Sprite &&s) :
+	width(s.width), height(s.height), angle(s.angle), stale(s.stale),
+	vertex_array(s.vertex_array), parent(s.parent), center(s.center),
+	pivot(s.pivot), position(s.position), scale(s.scale)
+{
+	s.vertex_array = nullptr;
+	s.parent = nullptr;
+}
 
 void Sprite::mirror()
 {

src/Graphics/Sprite.h
@@ -35,7 +35,7 @@ public:
 	const unsigned int height;  ///< Height of sprite (not scaled).
 
 	Sprite(const unsigned int width, const unsigned int height, const SpriteBatch *parent);
-	Sprite(const Sprite &, const SpriteBatch *parent);
+	Sprite(Sprite &&);
 
 	/// Return the current angle of the sprite.
 	inline float get_angle() const;

commit 7ed239db277eaf90ddee25fa0104137045916db9

Fixed Lua holding invalid pointers when SpriteBatch resizes.
Regressed in 81778f17.

src/Graphics/SpriteBatch.cpp
@@ -9,14 +9,14 @@ const char Drawable::class_name[] = "Drawable";
 SpriteBatch::SpriteBatch(const size_t hint) :
 	sprites(hint), vertices(hint * 4) { }
 
-Sprite& SpriteBatch::add(const int x, const int y, const int w, const int h)
+unsigned int SpriteBatch::add(const int x, const int y, const int w, const int h)
 {
-	Sprite &sprite = this->create_sprite(w, h);
-	sprite.set_texture(this->texture->define(Vec2i(x, y), w, h));
-	return sprite;
+	const unsigned int idx = this->create_sprite(w, h);
+	this->sprites[idx].set_texture(this->texture->define(Vec2i(x, y), w, h));
+	return idx;
 }
 
-Sprite& SpriteBatch::create_sprite(const unsigned int width, const unsigned int height)
+unsigned int SpriteBatch::create_sprite(const unsigned int width, const unsigned int height)
 {
 	R_ASSERT(this->sprites.size() <= static_cast<size_t>(Renderer::kNumSprites),
 	         "Hard-coded limit reached");
@@ -27,6 +27,7 @@ Sprite& SpriteBatch::create_sprite(const unsigned int width, const unsigned int 
 	this->vertices.push_back(SpriteVertex());
 	this->vertices.push_back(SpriteVertex());
 
+	const unsigned int idx = this->sprites.size();
 	this->sprites.push_back(Sprite(width, height, this));
 	this->array.count += 6;
 
@@ -38,13 +39,11 @@ Sprite& SpriteBatch::create_sprite(const unsigned int width, const unsigned int 
 	{
 		for (size_t i = 0; i < this->sprites.size(); ++i)
 			this->sprites[i].vertex_array = &this->vertices[i * 4];
-		return this->sprites.back();
 	}
+	else  // Assign the batch's buffer to the sprite.
+		this->sprites[idx].vertex_array = &this->vertices[this->vertices.size() - 4];
 
-	// Assign the batch's buffer to the sprite.
-	Sprite &sprite = this->sprites.back();
-	sprite.vertex_array = &this->vertices[this->vertices.size() - 4];
-	return sprite;
+	return idx;
 }
 
 TextureAtlas* SpriteBatch::set_texture(const DataMap &texture)

src/Graphics/SpriteBatch.h
@@ -33,20 +33,23 @@ public:
 	/// \param x,y     Position of the texture assigned to the sprite.
 	/// \param width   Width of the texture and, consequently, the sprite.
 	/// \param height  Height of the texture and, consequently, the sprite.
-	/// \return The newly created sprite, at position (0,0).
-	Sprite& add(const int x, const int y, const int width, const int height);
+	/// \return The index of the newly created sprite, positioned at (0,0).
+	unsigned int add(const int x, const int y, const int width, const int height);
 
 	/// Creates a sprite.
 	/// \param width   Width of the sprite.
 	/// \param height  Height of the sprite.
-	/// \return The newly created sprite, at position (0,0).
-	Sprite& create_sprite(const unsigned int width, const unsigned int height);
+	/// \return The index of the newly created sprite, positioned at (0,0).
+	unsigned int create_sprite(const unsigned int width, const unsigned int height);
 
 	/// Loads texture data.
 	TextureAtlas* set_texture(const DataMap &texture);
 
 	/// Reuses a texture atlas.
 	TextureAtlas* set_texture(TextureAtlas *texture);
+
+	/// Returns the sprite at index \p i.
+	inline Sprite& sprite(const unsigned int i) const;
 
 	/// Updates the batch of sprites.
 	void update();
@@ -58,5 +61,10 @@ private:
 	Vector<Sprite> sprites;           ///< Vector storing all sprites.
 	Vector<SpriteVertex> vertices;    ///< Common vertex array for all sprites in the batch.
 };
+
+Sprite& SpriteBatch::sprite(const unsigned int i) const
+{
+	return this->sprites[i];
+}
 
 #endif

src/Lua/lua_Sprite.cpp
@@ -1,4 +1,4 @@
-#include "Graphics/Sprite.h"
+#include "Graphics/SpriteBatch.h"
 #include "Lua/LuaHelper.h"
 #include "Lua/lua_Sprite.h"
 
@@ -6,13 +6,9 @@ namespace Rainbow
 {
 	namespace Lua
 	{
-		typedef Bind<Sprite, ::Sprite, kBindTypeWeak> LuaSprite;
+		const char Sprite::class_name[] = "sprite";
 
-		template<>
-		const char LuaSprite::class_name[] = "sprite";
-
-		template<>
-		const Method<Sprite> LuaSprite::methods[] = {
+		const Method<Sprite> Sprite::methods[] = {
 			{ "get_angle",    &Sprite::get_angle },
 			{ "get_color",    &Sprite::get_color },
 			{ "get_position", &Sprite::get_position },
@@ -29,17 +25,23 @@ namespace Rainbow
 		};
 
 		Sprite::Sprite(lua_State *L) :
-			Bind(static_cast< ::Sprite*>(lua_touserdata(L, -1))) { }
+			id(lua_tointeger(L, -1)),
+			batch(static_cast<SpriteBatch*>(lua_touserdata(L, -2))) { }
+
+		::Sprite* Sprite::get() const
+		{
+			return &this->batch->sprite(this->id);
+		}
 
 		int Sprite::get_angle(lua_State *L)
 		{
-			lua_pushnumber(L, this->ptr->get_angle());
+			lua_pushnumber(L, this->get()->get_angle());
 			return 1;
 		}
 
 		int Sprite::get_color(lua_State *L)
 		{
-			const Colorb& c = this->ptr->get_color();
+			const Colorb& c = this->get()->get_color();
 			lua_pushinteger(L, c.r);
 			lua_pushinteger(L, c.g);
 			lua_pushinteger(L, c.b);
@@ -49,7 +51,7 @@ namespace Rainbow
 
 		int Sprite::get_position(lua_State *L)
 		{
-			const Vec2f &v = this->ptr->get_position();
+			const Vec2f &v = this->get()->get_position();
 			lua_pushnumber(L, v.x);
 			lua_pushnumber(L, v.y);
 			return 2;
@@ -61,27 +63,25 @@ namespace Rainbow
 			color += luaR_tointeger(L, 2) << 16;
 			color += luaR_tointeger(L, 3) << 8;
 			color += luaR_optinteger(L, 4, 0xff);
-			this->ptr->set_color(color);
+			this->get()->set_color(color);
 			return 0;
 		}
 
 		int Sprite::set_pivot(lua_State *L)
 		{
-			const Vec2f pivot(luaR_tonumber(L, 1), luaR_tonumber(L, 2));
-			this->ptr->set_pivot(pivot);
+			this->get()->set_pivot(Vec2f(luaR_tonumber(L, 1), luaR_tonumber(L, 2)));
 			return 0;
 		}
 
 		int Sprite::set_position(lua_State *L)
 		{
-			const Vec2f position(luaR_tonumber(L, 1), luaR_tonumber(L, 2));
-			this->ptr->set_position(position);
+			this->get()->set_position(Vec2f(luaR_tonumber(L, 1), luaR_tonumber(L, 2)));
 			return 0;
 		}
 
 		int Sprite::set_rotation(lua_State *L)
 		{
-			this->ptr->set_rotation(luaR_tonumber(L, 1));
+			this->get()->set_rotation(luaR_tonumber(L, 1));
 			return 0;
 		}
 
@@ -90,10 +90,10 @@ namespace Rainbow
 			switch (lua_gettop(L))
 			{
 				case 2:
-					this->ptr->set_scale(Vec2f(luaR_tonumber(L, 1), luaR_tonumber(L, 2)));
+					this->get()->set_scale(Vec2f(luaR_tonumber(L, 1), luaR_tonumber(L, 2)));
 					break;
 				case 1:
-					this->ptr->set_scale(luaR_tonumber(L, 1));
+					this->get()->set_scale(luaR_tonumber(L, 1));
 					break;
 				default:
 					LUA_ASSERT(lua_gettop(L) == 1 || lua_gettop(L) == 2, "<sprite>:set_scale(fx [, fy])");
@@ -104,13 +104,13 @@ namespace Rainbow
 
 		int Sprite::set_texture(lua_State *L)
 		{
-			this->ptr->set_texture(luaR_tointeger(L, 1));
+			this->get()->set_texture(luaR_tointeger(L, 1));
 			return 0;
 		}
 
 		int Sprite::mirror(lua_State *)
 		{
-			this->ptr->mirror();
+			this->get()->mirror();
 			return 0;
 		}
 
@@ -118,8 +118,7 @@ namespace Rainbow
 		{
 			LUA_ASSERT(lua_gettop(L) == 2, "<sprite>:move(x, y)");
 
-			const Vec2f delta(luaR_tonumber(L, 1), luaR_tonumber(L, 2));
-			this->ptr->move(delta);
+			this->get()->move(Vec2f(luaR_tonumber(L, 1), luaR_tonumber(L, 2)));
 			return 0;
 		}
 
@@ -127,7 +126,7 @@ namespace Rainbow
 		{
 			LUA_ASSERT(lua_gettop(L) == 1, "<sprite>:rotate(r)");
 
-			this->ptr->rotate(luaR_tonumber(L, 1));
+			this->get()->rotate(luaR_tonumber(L, 1));
 			return 0;
 		}
 	}

src/Lua/lua_Sprite.h
@@ -4,20 +4,27 @@
 #include "Lua/LuaBind.h"
 
 struct lua_State;
-class  Sprite;
+class Sprite;
+class SpriteBatch;
 
 namespace Rainbow
 {
 	namespace Lua
 	{
-		class Sprite : public Bind<Sprite, ::Sprite, kBindTypeWeak>
+		class Sprite
 		{
-			friend class Bind<Sprite, ::Sprite, kBindTypeWeak>;
-
 		public:
+			static const char class_name[];
+			static const Method<Sprite> methods[];
+
 			Sprite(lua_State *);
 
+			::Sprite* get() const;
+
 		private:
+			const unsigned int id;
+			const SpriteBatch *batch;
+
 			int get_angle(lua_State *);
 			int get_color(lua_State *);
 			int get_position(lua_State *);

src/Lua/lua_SpriteBatch.cpp
@@ -33,7 +33,8 @@ namespace Rainbow
 			const int y = luaR_tointeger(L, 2);
 			const int w = luaR_tointeger(L, 3);
 			const int h = luaR_tointeger(L, 4);
-			lua_pushlightuserdata(L, &::SpriteBatch::add(x, y, w, h));
+			lua_pushlightuserdata(L, this);
+			lua_pushinteger(L, ::SpriteBatch::add(x, y, w, h));
 			return alloc<Sprite>(L);
 		}
 
@@ -43,7 +44,8 @@ namespace Rainbow
 
 			const int w = luaR_tointeger(L, 1);
 			const int h = luaR_tointeger(L, 2);
-			lua_pushlightuserdata(L, &::SpriteBatch::create_sprite(w, h));
+			lua_pushlightuserdata(L, this);
+			lua_pushinteger(L, ::SpriteBatch::create_sprite(w, h));
 			return alloc<Sprite>(L);
 		}
 