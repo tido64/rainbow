6b76fd939fa7ca6424ffdf92601f3ffd09b52669
Tommy Nguyen <tn0502@gmail.com>
2013-11-30 15:32:52 +0100

    iOS: Fixed AVAudioRecorder not recording on iOS 7.
    /dev/null is MIA on certain devices on iOS 7. We have to workaround this
    by saving to a temporary file instead.

--- src/ConFuoco/impl/Recorder_iOS.mm
+++ src/ConFuoco/impl/Recorder_iOS.mm
@@ -4,34 +4,44 @@
 #ifdef RAINBOW_IOS
 
 #include <AVFoundation/AVAudioRecorder.h>
+#include <AVFoundation/AVAudioSession.h>
 #include <CoreAudio/CoreAudioTypes.h>
 
 #include "Common/Algorithm.h"
+
+#define kTemporaryFilePath \
+	[NSTemporaryDirectory() stringByAppendingPathComponent:@"com.bifrostentertainment.maja"]
 
 namespace ConFuoco
 {
 	Recorder::Recorder() :
 		average(0.0f), low_pass(0.0f), peak(0.0f), recorder(nullptr)
 	{
-		NSURL *dev_null = [NSURL fileURLWithPath:@"/dev/null"];
+		NSString *path = kTemporaryFilePath;
+		if ([[NSFileManager defaultManager] fileExistsAtPath:path])
+		{
+			NSError *error;
+			[[NSFileManager defaultManager] removeItemAtPath:path error:&error];
+		}
 		NSDictionary *settings = [NSDictionary dictionaryWithObjectsAndKeys:
-			[NSNumber numberWithInt: kAudioFormatLinearPCM],  AVFormatIDKey,
-			[NSNumber numberWithFloat: 44100.0],              AVSampleRateKey,
-			[NSNumber numberWithInt: 1],                      AVNumberOfChannelsKey,
-			[NSNumber numberWithInt: 16],                     AVLinearPCMBitDepthKey,
-			[NSNumber numberWithInt: AVAudioQualityHigh],     AVEncoderAudioQualityKey,
+			[NSNumber numberWithInt:kAudioFormatLinearPCM],  AVFormatIDKey,
+			[NSNumber numberWithFloat:44100.0],              AVSampleRateKey,
+			[NSNumber numberWithInt:1],                      AVNumberOfChannelsKey,
+			[NSNumber numberWithInt:16],                     AVLinearPCMBitDepthKey,
+			[NSNumber numberWithInt:AVAudioQualityHigh],     AVEncoderAudioQualityKey,
 			nil];
 		NSError *error;
-		this->recorder = [[AVAudioRecorder alloc] initWithURL:dev_null
-		                                             settings:settings
-		                                                error:&error];
+		this->recorder = [[AVAudioRecorder alloc]
+				initWithURL:[NSURL fileURLWithPath:path isDirectory:NO]
+				   settings:settings
+				      error:&error];
 		if (!this->recorder)
-			NSLog(@"%@", [error description]);
-		else
 		{
-			[recorder setMeteringEnabled:YES];
-			[recorder prepareToRecord];
+			NSLog(@"%@", [error description]);
+			return;
 		}
+		[recorder setMeteringEnabled:YES];
+		[recorder prepareToRecord];
 	}
 
 	Recorder::~Recorder() { }
@@ -49,15 +59,30 @@ namespace ConFuoco
 	void Recorder::stop()
 	{
 		[recorder stop];
+		NSError *error;
+		[[NSFileManager defaultManager] removeItemAtPath:kTemporaryFilePath
+		                                           error:&error];
 	}
 
 	void Recorder::update()
 	{
-		[recorder updateMeters];
+		if (!recorder)
+			return;
 
+		NSError *error;
+		NSDictionary *attributes = [[NSFileManager defaultManager]
+				attributesOfItemAtPath:kTemporaryFilePath
+				                 error:&error];
+		if (attributes.fileSize >= 8 * 1024 * 1024)
+		{
+			this->stop();
+			this->record();
+			return;
+		}
+
+		[recorder updateMeters];
 		this->average = [recorder averagePowerForChannel:0];
 		this->peak = [recorder peakPowerForChannel:0];
-
 		this->low_pass = Rainbow::low_pass(this->peak, this->low_pass);
 	}
 }
