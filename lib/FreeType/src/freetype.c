8fdc1bad0736595cf9e97237822974f7979564ef
Tommy Nguyen <tn0502@gmail.com>
2013-12-15 17:23:43 +0100

    FreeType: Updated to v2.5.2.

--- lib/FreeType/src/freetype.c
+++ lib/FreeType/src/freetype.c
@@ -1,6 +1,12 @@
 // This FreeType custom build will only support OpenType and TrueType
 
 #define FT2_BUILD_LIBRARY 1
+#if defined(__ANDROID__)  // Fixes "Error: width suffixes are invalid in ARM mode"
+#	define FT_CONFIG_OPTION_NO_ASSEMBLER
+#elif defined(__IPHONE_OS_VERSION_MIN_REQUIRED)
+#	define FT_CONFIG_OPTION_SYSTEM_ZLIB
+#endif
+#define FT_CONFIG_MODULES_H  <rainbow/ftmodule.h>
 
 // Base components
 #include "base/ftsystem.c"
@@ -22,5 +28,6 @@
 
 // Auxiliary modules
 #include "autofit/autofit.c"
+#include "gzip/ftgzip.c"
 #include "pshinter/pshinter.c"
 #include "psnames/psnames.c"
