0cb8db45f4bb02447571f9f719f8f064ec9cfb9e
Tommy Nguyen <tn0502@gmail.com>
2013-12-16 21:31:26 +0100

    Cleaned up and updated build scripts.

--- build/cmake/FindVorbisfile.cmake
+++ build/cmake/FindVorbisfile.cmake
@@ -3,10 +3,6 @@
 # This module defines
 # 	VORBISFILE_INCLUDE_DIR, where to find vorbisfile.h, etc.
 # 	VORBISFILE_LIBRARIES, the libraries to link against to use Vorbisfile.
-# 	VORBISFILE_DEFINITIONS - You should add_definitons(${VORBISFILE_DEFINITIONS}) before compiling code that includes Vorbisfile library files.
-# 	VORBISFILE_FOUND, If false, do not try to use Vorbisfile.
-# also defined, but not for general use are
-# 	VORBISFILE_LIBRARY, where to find the Vorbisfile library.
 
 find_path(VORBISFILE_INCLUDE_DIR "vorbis/vorbisfile.h"
 	/usr/include                     # Linux
@@ -16,8 +12,8 @@ find_path(VORBISFILE_INCLUDE_DIR "vorbis/vorbisfile.h"
 	/usr/x86_64-w64-mingw32/include  # MinGW-w64 x64
 	build/windows/include            # Windows
 )
-find_library(VORBISFILE_LIBRARY NAMES vorbisfile libvorbisfile libvorbisfile_static
-                                HINTS build/windows/lib)
+find_library(VORBISFILE_LIBRARIES NAMES vorbisfile libvorbisfile libvorbisfile_static
+                                  HINTS build/windows/lib)
 
 if(MINGW OR WIN32)
 	find_path(VORBIS_INCLUDE_DIR "vorbis/codec.h"
@@ -39,19 +35,19 @@ if(MINGW OR WIN32)
 	                         HINTS build/windows/lib)
 
 	set(VORBISFILE_INCLUDE_DIR ${VORBISFILE_INCLUDE_DIR} ${VORBIS_INCLUDE_DIR} ${OGG_INCLUDE_DIR})
-	set(VORBISFILE_LIBRARY ${VORBISFILE_LIBRARY} ${VORBIS_LIBRARY} ${OGG_LIBRARY})
+	set(VORBISFILE_LIBRARIES ${VORBISFILE_LIBRARIES} ${VORBIS_LIBRARY} ${OGG_LIBRARY})
 
 	# Force use of found libraries (because sometimes it just won't obey)
-	foreach(lib ${VORBISFILE_LIBRARY})
+	foreach(lib ${VORBISFILE_LIBRARIES})
 		get_filename_component(libname ${lib} NAME_WE)
 		add_library(${libname} STATIC IMPORTED)
 		set_target_properties(${libname} PROPERTIES IMPORTED_LOCATION ${lib})
 		list(APPEND VORBIS_LIBRARIES ${libname})
 	endforeach()
-	set(VORBISFILE_LIBRARY ${VORBIS_LIBRARIES})
+	set(VORBISFILE_LIBRARIES ${VORBIS_LIBRARIES})
 endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Vorbisfile DEFAULT_MSG VORBISFILE_LIBRARY VORBISFILE_INCLUDE_DIR)
+find_package_handle_standard_args(Vorbisfile DEFAULT_MSG VORBISFILE_LIBRARIES VORBISFILE_INCLUDE_DIR)
 
-mark_as_advanced(VORBISFILE_LIBRARY VORBISFILE_INCLUDE_DIR)
+mark_as_advanced(VORBISFILE_LIBRARIES VORBISFILE_INCLUDE_DIR)
