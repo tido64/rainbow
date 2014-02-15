012ad2b65d353fb08c3b37bd1e657c28e51a9cd6
Tommy Nguyen <tn0502@gmail.com>
2014-02-15 20:05:01 +0100

    Windows: Invoke MSBuild through CMake.

--- tools/build.bat
+++ tools/build.bat
@@ -9,4 +9,4 @@ SET SOURCE_DIR=%~dp0\..
 DEL /S /Q *.vcxproj *.vcxproj.filters *.vcxproj.user CMakeCache.txt cmake_install.cmake Rainbow.sdf Rainbow.sln 1> NUL 2>&1
 RD /S /Q box2d.dir CMakeFiles Debug freetype.dir GLee.dir ipch lib lua.dir png.dir rainbow.dir Release Win32 1> NUL 2>&1
 cmake %* -G %MSVC_VER% %SOURCE_DIR%
-MSBuild Rainbow.sln /property:Configuration=Debug /maxcpucount
+cmake --build . --config Debug -- /maxcpucount
