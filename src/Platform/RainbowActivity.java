7b12b7630e8795e168d64e52a98c9cfa0110615f
Tommy Nguyen <tn0502@gmail.com>
2014-03-06 00:32:45 +0100

    Android: Implemented immersive full-screen mode.

--- src/Platform/RainbowActivity.java
+++ src/Platform/RainbowActivity.java
@@ -14,52 +14,78 @@ import android.view.View;
 
 public class RainbowActivity extends NativeActivity
                              implements View.OnSystemUiVisibilityChangeListener {
-	private static final long kDimmerDelay = 5000;
+    static {
+        System.loadLibrary("gnustl_shared");
+    }
 
-	private Handler mDimmerHandler;
-	private Runnable mDimmerRunnable;
-	private View mContentView;
+    /* NativeActivity */
 
-	static {
-		System.loadLibrary("gnustl_shared");
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (kShouldDimSystemUI) {
+            mDimmerHandler = new Handler();
+            mDimmerRunnable = new Runnable() {
+                public void run() {
+                    dimSystemUI();
+                }
+            };
+            contentView().setOnSystemUiVisibilityChangeListener(this);
+        }
+    }
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
-			return;
-		}
-		mDimmerHandler = new Handler();
-		mDimmerRunnable = new Runnable() {
-			public void run() {
-				dimSystemUi();
-			}
-		};
-		mContentView = getWindow().getDecorView().findViewById(android.R.id.content);
-		mContentView.setOnSystemUiVisibilityChangeListener(this);
-	}
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (!hasFocus)
+            return;
+        if (!kSupportsImmersiveFullScreenMode) {
+            if (kSupportsLowProfileSystemUI) {
+                mDimmerHandler.removeCallbacks(mDimmerRunnable);
+                dimSystemUI();
+            }
+            return;
+        }
+        contentView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
 
-	@Override
-	public void onResume() {
-		super.onResume();
-		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
-			return;
-		}
-		mDimmerHandler.removeCallbacks(mDimmerRunnable);
-		dimSystemUi();
-	}
+    /* View.OnSystemUiVisibilityChangeListener */
 
-	@Override
-	public void onSystemUiVisibilityChange(int visibility) {
-		if ((visibility & View.SYSTEM_UI_FLAG_LOW_PROFILE) != 0) {
-			return;
-		}
-		mDimmerHandler.removeCallbacks(mDimmerRunnable);
-		mDimmerHandler.postDelayed(mDimmerRunnable, kDimmerDelay);
-	}
+    @Override
+    public void onSystemUiVisibilityChange(int visibility) {
+        assert kShouldDimSystemUI;
+        if ((visibility & View.SYSTEM_UI_FLAG_LOW_PROFILE) != 0) {
+            return;
+        }
+        mDimmerHandler.removeCallbacks(mDimmerRunnable);
+        mDimmerHandler.postDelayed(mDimmerRunnable, kDimmerDelay);
+    }
 
-	private void dimSystemUi() {
-		mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
-	}
+    /* Private */
+
+    private static final boolean kSupportsImmersiveFullScreenMode =
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+    private static final boolean kSupportsLowProfileSystemUI =
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN;
+    private static final boolean kShouldDimSystemUI =
+            !kSupportsImmersiveFullScreenMode && kSupportsLowProfileSystemUI;
+    private static final long kDimmerDelay = 5000;
+
+    private Handler mDimmerHandler;
+    private Runnable mDimmerRunnable;
+
+    private View contentView() {
+        return getWindow().getDecorView().findViewById(android.R.id.content);
+    }
+
+    private void dimSystemUI() {
+        assert kShouldDimSystemUI;
+        contentView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
+    }
 }
