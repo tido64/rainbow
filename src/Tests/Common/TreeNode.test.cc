c530f0e155a493e20472ef63ccac074e6565a82e
Tommy Nguyen <tn0502@gmail.com>
2015-09-17 00:38:14 +0200

    Tests for TreeNode move construction/assignment and traversing.

--- src/Tests/Common/TreeNode.test.cc
+++ src/Tests/Common/TreeNode.test.cc
@@ -12,10 +12,16 @@ namespace
 	{
 	public:
 		TreeNodeTest(bool &deleted) : deleted_(deleted) { deleted_ = false; }
+		TreeNodeTest(TreeNodeTest&&) = default;
 		~TreeNodeTest() { deleted_ = true; }
 
 		const std::vector<TreeNodeTest*>& children() const { return children_; }
-		TreeNodeTest* parent() const { return parent_; }
+
+		TreeNodeTest& operator=(TreeNodeTest&& node)
+		{
+			TreeNode::operator=(std::move(node));
+			return *this;
+		}
 
 	private:
 		bool &deleted_;
@@ -28,6 +34,38 @@ TEST_CASE("A newly constructed node is always a free node", "[tree]")
 	TreeNodeTest node(node_deleted);
 	REQUIRE_FALSE(node.parent());
 	REQUIRE(node.children().empty());
+}
+
+TEST_CASE("TreeNodes can be moved", "[tree]")
+{
+	bool root_deleted = false;
+	TreeNodeTest root(root_deleted);
+
+	bool node_deleted = false;
+	TreeNodeTest *node = new TreeNodeTest(node_deleted);
+	root.add_child(node);
+
+	REQUIRE_FALSE(root.children().empty());
+
+	SECTION("Can be move constructed")
+	{
+		TreeNodeTest new_root(std::move(root));
+
+		REQUIRE_FALSE(node_deleted);
+		REQUIRE(new_root.children().at(0) == node);
+		REQUIRE(root.children().empty());
+	}
+
+	SECTION("Can be move assigned")
+	{
+		bool new_root_deleted = false;
+		TreeNodeTest new_root(new_root_deleted);
+		new_root = std::move(root);
+
+		REQUIRE_FALSE(node_deleted);
+		REQUIRE(new_root.children().at(0) == node);
+		REQUIRE(root.children().empty());
+	}
 }
 
 TEST_CASE("Adding/removing nodes", "[tree]")
@@ -189,4 +227,39 @@ TEST_CASE("Removing a node will also remove its children", "[tree]")
 	REQUIRE(deleted[0]);
 	REQUIRE(deleted[1]);
 	REQUIRE(root.children().empty());
+}
+
+TEST_CASE("Traverse nodes with for_each", "[tree]")
+{
+	bool root_deleted = false;
+	TreeNodeTest root(root_deleted);
+
+	bool deleted[]{false, false, false, false, false};
+	TreeNodeTest* nodes[]{
+	    new TreeNodeTest(deleted[0]),
+	    new TreeNodeTest(deleted[1]),
+	    new TreeNodeTest(deleted[2]),
+	    new TreeNodeTest(deleted[3]),
+	    new TreeNodeTest(deleted[4])};
+
+	root.add_child(nodes[0]);
+	nodes[0]->add_child(nodes[1]);
+	nodes[0]->add_child(nodes[2]);
+	nodes[2]->add_child(nodes[3]);
+	nodes[2]->add_child(nodes[4]);
+
+	for_each(&root, [&nodes](const TreeNodeTest *node) {
+		for (size_t i = 0; i < rainbow::array_size(nodes); ++i)
+		{
+			if (nodes[i] == node)
+			{
+				nodes[i] = nullptr;
+				break;
+			}
+		}
+	});
+
+	REQUIRE(std::all_of(nodes, std::end(nodes), [](const TreeNodeTest *node) {
+		return node == nullptr;
+	}));
 }
