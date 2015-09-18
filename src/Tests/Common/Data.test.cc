4a7e66b57d2fb74f4909546723db18cee0659b4d
Tommy Nguyen <tn0502@gmail.com>
2015-09-18 23:39:20 +0200

    Added test for Data::from_literal.

--- src/Tests/Common/Data.test.cc
+++ src/Tests/Common/Data.test.cc
@@ -6,18 +6,32 @@
 
 #include <catch.hpp>
 
+#include "Common/Algorithm.h"
 #include "Common/Data.h"
 #include "FileSystem/Path.h"
 
+namespace
+{
+	const char kSecretData[] = "It's a double-rainbow!\n";
+}
+
+TEST_CASE("Data can reference in-memory data", "[data]")
+{
+	Data blob = Data::from_literal(kSecretData);
+	REQUIRE(blob.bytes() == kSecretData);
+	REQUIRE(blob.size() == rainbow::array_size(kSecretData) - 1);
+	REQUIRE(memcmp(blob, kSecretData, blob.size()) == 0);
+}
+
 TEST_CASE("Data manages data and performs disk operations", "[data]")
 {
-	const char secret[] = "It's a double-rainbow!\n";
-	Data blob(secret, strlen(secret), Data::Ownership::Reference);
+	Data blob(kSecretData, rainbow::array_size(kSecretData),
+	          Data::Ownership::Reference);
 
 	SECTION("Data manages arbitrary data blobs")
 	{
-		REQUIRE(blob.size() == strlen(secret));
-		REQUIRE(strcmp(blob, secret) == 0);
+		REQUIRE(blob.size() == rainbow::array_size(kSecretData));
+		REQUIRE(strcmp(blob, kSecretData) == 0);
 	}
 
 	SECTION("Data writes and reads from disk")
@@ -27,7 +41,7 @@ TEST_CASE("Data manages data and performs disk operations", "[data]")
 		REQUIRE(blob.save(file));
 		const Data &from_disk = Data::load_document(file);
 		REQUIRE(from_disk);
-		REQUIRE(strcmp(from_disk, secret) == 0);
+		REQUIRE(strcmp(from_disk, kSecretData) == 0);
 		Path path(file, Path::RelativeTo::UserDataPath);
 		remove(path);
 	}
