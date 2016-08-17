9e7b4dcb90975090ee7c6ecac22fb4fa7727678b
Tommy Nguyen <tn0502@gmail.com>
2016-08-18 00:40:41 +0200

    Added VirtualKey test.

---
+++ src/Tests/Input/VirtualKey.test.cc
@@ -0,0 +1,30 @@
+// Copyright (c) 2010-16 Bifrost Entertainment AS and Tommy Nguyen
+// Distributed under the MIT License.
+// (See accompanying file LICENSE or copy at http://opensource.org/licenses/MIT)
+
+#include "Input/VirtualKey.h"
+
+#include "Platform/Macros.h"
+#ifdef RAINBOW_SDL
+#   include <SDL_config.h>
+#   include <SDL_keyboard.h>
+#endif  // RAINBOW_SDL
+
+#include <gtest/gtest.h>
+
+using rainbow::VirtualKey;
+
+TEST(VirtualKeyTest, ScancodeToVirtualKeyMappingIsConsistent)
+{
+    for (int i = static_cast<int>(VirtualKey::Unknown);
+         i < static_cast<int>(VirtualKey::KeyCount);
+         ++i)
+    {
+        const VirtualKey vk = static_cast<VirtualKey>(i);
+#ifdef RAINBOW_SDL
+        SDL_Keysym keysym{};
+        keysym.scancode = static_cast<SDL_Scancode>(rainbow::to_scancode(vk));
+#endif  // RAINBOW_SDL
+        ASSERT_EQ(vk, rainbow::to_virtualkey(keysym));
+    }
+}
