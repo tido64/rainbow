af0b415a2cafd2a19affba5b3616e671717bdf0d
Tommy Nguyen <tn0502@gmail.com>
2018-03-25 18:00:23 +0200

    Refactored Data tests.

--- src/Tests/Common/Data.test.cc
+++ src/Tests/Common/Data.test.cc
@@ -14,20 +14,46 @@ using rainbow::Data;
 namespace
 {
     constexpr char kSecretData[] = "It's a double-rainbow!\n";
-}
 
-TEST(DataTest, ReferencesInMemoryData)
-{
-    const Data& blob = Data::from_literal(kSecretData);
-    ASSERT_EQ(kSecretData, blob.as<char*>());
-    ASSERT_EQ(rainbow::array_size(kSecretData) - 1, blob.size());
-    ASSERT_EQ(0, memcmp(blob.bytes(), kSecretData, blob.size()));
-}
+    struct FromBytes
+    {
+        auto size() { return rainbow::array_size(kSecretData); }
+        auto operator()() -> Data { return Data::from_bytes(kSecretData); }
+    };
 
-TEST(DataTest, ManagesArbitraryDataBlobs)
+    struct FromLiteral
+    {
+        auto size() { return rainbow::array_size(kSecretData) - 1; }
+        auto operator()() -> Data { return Data::from_literal(kSecretData); }
+    };
+
+    template <typename T>
+    class DataTest : public ::testing::Test
+    {
+    protected:
+        T data_;
+    };
+
+    using DataTestTypes = ::testing::Types<FromBytes, FromLiteral>;
+}  // namespace
+
+TYPED_TEST_CASE(DataTest, DataTestTypes);
+
+TYPED_TEST(DataTest, EncapsulatesInMemoryData)
 {
-    const Data& blob = Data::from_bytes(kSecretData);
+    Data blob = this->data_();
+
+    ASSERT_TRUE(blob);
     ASSERT_EQ(kSecretData, blob.as<char*>());
-    ASSERT_EQ(rainbow::array_size(kSecretData), blob.size());
+    ASSERT_EQ(this->data_.size(), blob.size());
     ASSERT_EQ(0, memcmp(blob.bytes(), kSecretData, blob.size()));
+
+    Data moved_blob{std::move(blob)};
+
+    ASSERT_FALSE(blob);
+    ASSERT_EQ(nullptr, blob.bytes());
+    ASSERT_EQ(0u, blob.size());
+    ASSERT_EQ(kSecretData, moved_blob.as<char*>());
+    ASSERT_EQ(this->data_.size(), moved_blob.size());
+    ASSERT_EQ(0, memcmp(moved_blob.bytes(), kSecretData, moved_blob.size()));
 }
