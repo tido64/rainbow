0ac7d8628a5c59194decfd387db214c0606a7965
Tommy Nguyen <tn0502@gmail.com>
2018-11-02 22:44:09 +0100

    FreeType: Upgraded to v2.9.1.

--- src/ThirdParty/FreeType/freetype.c
+++ src/ThirdParty/FreeType/freetype.c
@@ -7,10 +7,8 @@
 #include <base/ftbase.c>
 #include <base/ftbbox.c>
 #include <base/ftbitmap.c>
-#include <base/ftfntfmt.c>
 #include <base/ftglyph.c>
 #include <base/ftinit.c>
-#include <base/ftlcdfil.c>
 #include <base/ftsystem.c>
 #include <cff/cff.c>
 #include <gzip/ftgzip.c>
