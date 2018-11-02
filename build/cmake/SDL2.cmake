ddbb0bf19688248e09722ae71aa99f09a685ed18
Tommy Nguyen <tn0502@gmail.com>
2018-11-02 21:03:27 +0100

    SDL: Upgraded to v2.0.9.

--- build/cmake/SDL2.cmake
+++ build/cmake/SDL2.cmake
@@ -1,8 +1,6 @@
-set(SDL2_TARGET_VERSION 2.0.7)
-set(SDL2_DEV_HASH 572dbed4ddc236498e53bd93092d677f65cfc8faec64e0b99e8752f6358efcc1)
-set(SDL2_BIN32_HASH 9f4de98f10be556c4ecba31d5897bd3b9778f130cf5c05e0f0b8fe241ea62831)
-set(SDL2_BIN64_HASH 444ae8b358f50d8f118163a89d886d452ff6cfbfaf2ef31cb10574604929f960)
-set(SDL2_SOURCE_DIR ${LOCAL_LIBRARY}/SDL)
+set(SDL2_TARGET_VERSION 2.0.9)
+set(SDL2_DEV_HASH ea266ef613f88433f493498f9e72e6bed5d03e4f3fde5b571a557a754ade9065)
+set(SDL2_BIN_HASH 8858e498f68c9c31f0eed0a21014befd63254aa52a284fb779c5150f2659d2be)
 
 if(ANDROID)
   # Pass through.
@@ -10,17 +8,9 @@ elseif(EMSCRIPTEN)
   set(SDL2_INCLUDE_DIRS ${LOCAL_LIBRARY}/SDL/include)
   set_target_properties(rainbow PROPERTIES LINK_FLAGS "-s DEMANGLE_SUPPORT=1 -s USE_SDL=2")
 elseif(MSVC)
-  if(CMAKE_CL_64)
-    set(SDL2_ARCH x64)
-    set(SDL2_BIN_HASH ${SDL2_BIN64_HASH})
-  else()
-    set(SDL2_ARCH x86)
-    set(SDL2_BIN_HASH ${SDL2_BIN32_HASH})
-  endif()
-
   download_library(
       sdl2-bin
-      https://libsdl.org/release/SDL2-${SDL2_TARGET_VERSION}-win32-${SDL2_ARCH}.zip
+      https://libsdl.org/release/SDL2-${SDL2_TARGET_VERSION}-win32-x64.zip
       ${SDL2_BIN_HASH}
   )
   copy_to_build_dir(sdl2-bin SDL2.dll SDL2.dll)
@@ -35,7 +25,7 @@ elseif(MSVC)
   add_dependencies(rainbow sdl2-dev)
 
   target_include_directories(rainbow PRIVATE ${SOURCE_DIR}/include)
-  set(SDL2_LIBRARY_DIR ${SOURCE_DIR}/lib/${SDL2_ARCH})
+  set(SDL2_LIBRARY_DIR ${SOURCE_DIR}/lib/x64)
   target_link_libraries(rainbow ${SDL2_LIBRARY_DIR}/SDL2.lib ${SDL2_LIBRARY_DIR}/SDL2main.lib)
 elseif(UNIX)
   pkg_check_modules(SDL2 REQUIRED sdl2)
