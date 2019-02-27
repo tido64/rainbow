ea395b6e34d8780d515293a45e8f17ab9c8c33cc
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2019-02-27 08:24:41 +0100

    DO NOT MERGE: List *.gcov files

--- tools/build-ci.sh
+++ tools/build-ci.sh
@@ -57,6 +57,7 @@ case $1 in
     case $TARGET_OS in
       "Linux")
         cd out
+        find CMakeFiles -name *.gcov
         bash <(curl -s https://codecov.io/bash) -R .. -x gcov-7 -K
         ;;
     esac
