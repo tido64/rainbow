c66bbdfa93154ecf7df9686136e10265498c8dd3
Tommy Nguyen <tn0502@gmail.com>
2019-03-30 19:03:04 +0100

    Added more tests.

--- src/Tests/Script/JS/Module.test.cc
+++ src/Tests/Script/JS/Module.test.cc
@@ -6,24 +6,71 @@
 
 #include <gtest/gtest.h>
 
-#include "Script/JavaScript/JavaScript.h"
+#include "Tests/TestHelpers.h"
 
 namespace
 {
     namespace duk = rainbow::duk;
 
+    void on_fatal(void*, const char* msg) { throw std::runtime_error(msg); }
+
     class JSModuleTest : public ::testing::Test
     {
+    public:
+        JSModuleTest()
+            : context_(
+                  duk_create_heap(nullptr, nullptr, nullptr, this, &on_fatal))
+        {
+        }
+
+        ~JSModuleTest() { duk_destroy_heap(context_); }
+
     protected:
-        duk::Context context_;
+        duk_context* context_;
     };
 }  // namespace
+
+TEST_F(JSModuleTest, Initializes)
+{
+    duk::module::initialize(context_);
+    ASSERT_EQ(duk_get_top(context_), 0);
+}
+
+TEST_F(JSModuleTest, ThrowsLoadingEmptyModuleName)
+{
+    duk_push_literal(context_, "");
+
+    ASSERT_THROW(duk::module::load(context_), std::runtime_error);
+    ASSERT_TRUE(duk_is_error(context_, 1));
+}
+
+TEST_F(JSModuleTest, LoadsModules)
+{
+    constexpr char kTestModule[] = "rainbow-test-js-module-loads-modules.js";
+    rainbow::test::ScopedFile module(kTestModule);
+
+    duk_push_literal(context_, kTestModule);
+    duk::module::load(context_);
+
+    ASSERT_STREQ(duk_get_string(context_, 1), kTestModule);
+}
+
+TEST_F(JSModuleTest, ThrowsResolvingEmptyModuleName)
+{
+    duk_push_literal(context_, "");
+    duk_push_literal(context_, "");
+
+    ASSERT_THROW(duk::module::resolve(context_), std::runtime_error);
+    ASSERT_TRUE(duk_is_error(context_, 2));
+}
 
 TEST_F(JSModuleTest, ResolvesMainModuleRelativePaths)
 {
     duk_push_literal(context_, "requested");
     duk_push_literal(context_, "");
-    duk::module::resolve(context_);
+
+    ASSERT_EQ(duk::module::resolve(context_), 1);
+
     auto result = duk_get_string(context_, 2);
 
     ASSERT_STREQ(result, "requested.js");
@@ -31,7 +78,9 @@ TEST_F(JSModuleTest, ResolvesMainModuleRelativePaths)
     duk_set_top(context_, 0);
     duk_push_literal(context_, "requested.js");
     duk_push_literal(context_, "");
-    duk::module::resolve(context_);
+
+    ASSERT_EQ(duk::module::resolve(context_), 1);
+
     result = duk_get_string(context_, 2);
 
     ASSERT_STREQ(result, "requested.js");
@@ -39,7 +88,9 @@ TEST_F(JSModuleTest, ResolvesMainModuleRelativePaths)
     duk_set_top(context_, 0);
     duk_push_literal(context_, "requested");
     duk_push_literal(context_, "parent");
-    duk::module::resolve(context_);
+
+    ASSERT_EQ(duk::module::resolve(context_), 1);
+
     result = duk_get_string(context_, 2);
 
     ASSERT_STREQ(result, "requested.js");
@@ -47,7 +98,9 @@ TEST_F(JSModuleTest, ResolvesMainModuleRelativePaths)
     duk_set_top(context_, 0);
     duk_push_literal(context_, "requested");
     duk_push_literal(context_, "grandparent/parent");
-    duk::module::resolve(context_);
+
+    ASSERT_EQ(duk::module::resolve(context_), 1);
+
     result = duk_get_string(context_, 2);
 
     ASSERT_STREQ(result, "requested.js");
@@ -57,7 +110,9 @@ TEST_F(JSModuleTest, ResolvesRelativePaths)
 {
     duk_push_literal(context_, "./requested");
     duk_push_literal(context_, "");
-    duk::module::resolve(context_);
+
+    ASSERT_EQ(duk::module::resolve(context_), 1);
+
     auto result = duk_get_string(context_, 2);
 
     ASSERT_STREQ(result, "./requested.js");
@@ -65,7 +120,9 @@ TEST_F(JSModuleTest, ResolvesRelativePaths)
     duk_set_top(context_, 0);
     duk_push_literal(context_, "./requested.js");
     duk_push_literal(context_, "");
-    duk::module::resolve(context_);
+
+    ASSERT_EQ(duk::module::resolve(context_), 1);
+
     result = duk_get_string(context_, 2);
 
     ASSERT_STREQ(result, "./requested.js");
@@ -73,7 +130,9 @@ TEST_F(JSModuleTest, ResolvesRelativePaths)
     duk_set_top(context_, 0);
     duk_push_literal(context_, "../requested");
     duk_push_literal(context_, "");
-    duk::module::resolve(context_);
+
+    ASSERT_EQ(duk::module::resolve(context_), 1);
+
     result = duk_get_string(context_, 2);
 
     ASSERT_STREQ(result, "../requested.js");
@@ -81,7 +140,9 @@ TEST_F(JSModuleTest, ResolvesRelativePaths)
     duk_set_top(context_, 0);
     duk_push_literal(context_, "./requested");
     duk_push_literal(context_, "parent");
-    duk::module::resolve(context_);
+
+    ASSERT_EQ(duk::module::resolve(context_), 1);
+
     result = duk_get_string(context_, 2);
 
     ASSERT_STREQ(result, "./requested.js");
@@ -89,7 +150,9 @@ TEST_F(JSModuleTest, ResolvesRelativePaths)
     duk_set_top(context_, 0);
     duk_push_literal(context_, "./requested");
     duk_push_literal(context_, "./parent");
-    duk::module::resolve(context_);
+
+    ASSERT_EQ(duk::module::resolve(context_), 1);
+
     result = duk_get_string(context_, 2);
 
     ASSERT_STREQ(result, "./requested.js");
@@ -97,7 +160,9 @@ TEST_F(JSModuleTest, ResolvesRelativePaths)
     duk_set_top(context_, 0);
     duk_push_literal(context_, "./requested");
     duk_push_literal(context_, "./grandparent/parent");
-    duk::module::resolve(context_);
+
+    ASSERT_EQ(duk::module::resolve(context_), 1);
+
     result = duk_get_string(context_, 2);
 
     ASSERT_STREQ(result, "./grandparent/requested.js");
@@ -105,7 +170,9 @@ TEST_F(JSModuleTest, ResolvesRelativePaths)
     duk_set_top(context_, 0);
     duk_push_literal(context_, "./requested");
     duk_push_literal(context_, "../grandparent/parent");
-    duk::module::resolve(context_);
+
+    ASSERT_EQ(duk::module::resolve(context_), 1);
+
     result = duk_get_string(context_, 2);
 
     ASSERT_STREQ(result, "../grandparent/requested.js");
@@ -113,7 +180,9 @@ TEST_F(JSModuleTest, ResolvesRelativePaths)
     duk_set_top(context_, 0);
     duk_push_literal(context_, "../requested");
     duk_push_literal(context_, "./grandparent/parent");
-    duk::module::resolve(context_);
+
+    ASSERT_EQ(duk::module::resolve(context_), 1);
+
     result = duk_get_string(context_, 2);
 
     ASSERT_STREQ(result, "./requested.js");
