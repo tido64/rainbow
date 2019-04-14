cf166dc75f380bfe5174c60b9cc1683a2d3e7442
Tommy Nguyen <tn0502@gmail.com>
2019-04-14 12:40:16 +0200

    Added more VirtualKey tests.

--- src/Tests/Input/VirtualKey.test.cc
+++ src/Tests/Input/VirtualKey.test.cc
@@ -6,27 +6,87 @@
 
 #include "Platform/Macros.h"
 #ifdef RAINBOW_SDL
-#    include <SDL_config.h>
-#    include <SDL_keyboard.h>
+#    include <SDL_config.h>  // Ensure we include the correct SDL_config.h.
+#    include <SDL.h>
 #endif  // RAINBOW_SDL
 
 #include <gtest/gtest.h>
 
 #include "Common/TypeCast.h"
 
-using rainbow::VirtualKey;
+using namespace rainbow;
 
-TEST(VirtualKeyTest, ScancodeToVirtualKeyMappingIsConsistent)
-{
-    for (auto i = rainbow::to_underlying_type(VirtualKey::Unknown);
-         i < rainbow::to_underlying_type(VirtualKey::KeyCount);
-         ++i)
-    {
-        const auto vk = static_cast<VirtualKey>(i);
 #ifdef RAINBOW_SDL
+TEST(VirtualKeyTest, DISABLED_MapsKeyCode)
+{
+    // Only SDL_VideoInit() will call SDL_KeyboardInit(). If video
+    // initialisation fails, we can't test to_keycode().
+    if (SDL_VideoInit(nullptr) == 0)
+    {
+        const auto begin = to_underlying_type(VirtualKey::Unknown) + 1;
+        const auto end = to_underlying_type(VirtualKey::KeyCount);
+        for (auto i = begin; i < end; ++i)
+        {
+            const auto vk = static_cast<VirtualKey>(i);
+            ASSERT_EQ(SDL_GetScancodeFromKey(to_keycode(vk)),
+                      static_cast<SDL_Scancode>(to_scancode(vk)));
+        }
+    }
+
+    SDL_Quit();
+}
+
+TEST(VirtualKeyTest, MapsKeyMods)
+{
+    constexpr std::tuple<int, uint32_t> data_set[]{
+        {KMOD_NONE, KeyMods::None},
+        {KMOD_LSHIFT, KeyMods::LeftShift},
+        {KMOD_RSHIFT, KeyMods::RightShift},
+        {KMOD_LCTRL, KeyMods::LeftCtrl},
+        {KMOD_RCTRL, KeyMods::RightCtrl},
+        {KMOD_LALT, KeyMods::LeftAlt},
+        {KMOD_RALT, KeyMods::RightAlt},
+        {KMOD_LGUI, KeyMods::LeftSuper},
+        {KMOD_RGUI, KeyMods::RightSuper},
+        {KMOD_NUM, KeyMods::KeyMods::None},   // Unimplemented
+        {KMOD_CAPS, KeyMods::KeyMods::None},  // Unimplemented
+        {KMOD_MODE, KeyMods::KeyMods::None},  // Unimplemented
+        // Left + right combinations
+        {KMOD_CTRL, KeyMods::Ctrl},
+        {KMOD_SHIFT, KeyMods::Shift},
+        {KMOD_ALT, KeyMods::Alt},
+        {KMOD_GUI, KeyMods::Super},
+        // Mixed combinations
+        {KMOD_LSHIFT | KMOD_LCTRL, KeyMods::LeftShift | KeyMods::LeftCtrl},
+        {KMOD_LSHIFT | KMOD_LALT, KeyMods::LeftShift | KeyMods::LeftAlt},
+        {KMOD_LCTRL | KMOD_LALT, KeyMods::LeftCtrl | KeyMods::LeftAlt},
+        {KMOD_LSHIFT | KMOD_LCTRL | KMOD_LALT,
+         KeyMods::LeftShift | KeyMods::LeftCtrl | KeyMods::LeftAlt},
+        {KMOD_LSHIFT | KMOD_LGUI, KeyMods::LeftShift | KeyMods::LeftSuper},
+        {KMOD_LSHIFT | KMOD_LALT, KeyMods::LeftShift | KeyMods::LeftAlt},
+        {KMOD_LGUI | KMOD_LALT, KeyMods::LeftSuper | KeyMods::LeftAlt},
+        {KMOD_LSHIFT | KMOD_LGUI | KMOD_LALT,
+         KeyMods::LeftShift | KeyMods::LeftSuper | KeyMods::LeftAlt},
+    };
+
+    for (auto [mod, expected] : data_set)
+    {
         SDL_Keysym keysym{};
-        keysym.scancode = static_cast<SDL_Scancode>(rainbow::to_scancode(vk));
-#endif  // RAINBOW_SDL
-        ASSERT_EQ(rainbow::to_virtualkey(keysym), vk);
+        keysym.mod = mod;
+        ASSERT_EQ(KeyStroke::from_event(keysym).mods, expected);
     }
 }
+
+TEST(VirtualKeyTest, MapsScanCode)
+{
+    const auto begin = to_underlying_type(VirtualKey::Unknown);
+    const auto end = to_underlying_type(VirtualKey::KeyCount);
+    for (auto i = begin; i < end; ++i)
+    {
+        const auto vk = static_cast<VirtualKey>(i);
+        SDL_Keysym keysym{};
+        keysym.scancode = static_cast<SDL_Scancode>(to_scancode(vk));
+        ASSERT_EQ(to_virtualkey(keysym), vk);
+    }
+}
+#endif  // RAINBOW_SDL
