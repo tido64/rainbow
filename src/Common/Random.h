e0082dac2d3ed4e9396fc5da2378cfdaf7e1637c
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2019-04-15 11:35:04 +0200

    Added support for Visual Studio 2019. (#27)

--- src/Common/Random.h
+++ src/Common/Random.h
@@ -111,41 +111,52 @@ namespace xorshift
 
     private:
         static constexpr uint64_t kJump[]{
-            0x84242f96eca9c41d, 0xa3c65b8776f96855, 0x5b34a39f070b5837,
-            0x4489affce4f31a1e, 0x2ffeeb0a48316f40, 0xdc2d9891fe68c022,
-            0x3659132bb12fea70, 0xaac17d8efa43cab8, 0xc4cb815590989b13,
-            0x5ee975283d71c93b, 0x691548c86c1bd540, 0x7910c41d10a1e6a5,
-            0x0b5fc64563b3e2a8, 0x047f7684e9fc949d, 0xb99181f2d8f685ca,
-            0x284600e3f30e38c3};
+            0x84242f96eca9c41d,
+            0xa3c65b8776f96855,
+            0x5b34a39f070b5837,
+            0x4489affce4f31a1e,
+            0x2ffeeb0a48316f40,
+            0xdc2d9891fe68c022,
+            0x3659132bb12fea70,
+            0xaac17d8efa43cab8,
+            0xc4cb815590989b13,
+            0x5ee975283d71c93b,
+            0x691548c86c1bd540,
+            0x7910c41d10a1e6a5,
+            0x0b5fc64563b3e2a8,
+            0x047f7684e9fc949d,
+            0xb99181f2d8f685ca,
+            0x284600e3f30e38c3,
+        };
 
         uint64_t s_[16];
         int p_ = 0;
     };
-}
+}  // namespace xorshift
 
 namespace rainbow
 {
     /// <summary>User-friendly wrapper for xorshift1024* generator.</summary>
     struct Random
     {
-        std::uniform_real_distribution<double> dis;
+        std::uniform_real_distribution<double> dist;
         xorshift::xorshift1024star gen;
 
         void seed(uint64_t seed = 0) { gen.seed(seed); }
 
-        auto operator()() { return dis(gen); }
+        auto operator()() { return dist(gen); }
 
         template <typename N>
         auto operator()(N n) -> N
         {
-            return (*this)() * n;
+            return dist(gen) * n;
         }
 
         template <typename N>
         auto operator()(N m, N n) -> N
         {
             R_ASSERT(m < n, "Parameters must be in ascending order");
-            return static_cast<N>((*this)() * (n - m) + m);
+            return static_cast<N>(dist(gen) * (n - m) + m);
         }
     };
 
