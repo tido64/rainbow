d490334d11b1e73b5164e170d5fb3954fc60a2ad
Tommy Nguyen <tn0502@gmail.com>
2019-06-15 22:35:56 +0200

    Removed unused scripts.

--- tools/update-glext.h.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2010-present Bifrost Entertainment AS and Tommy Nguyen
-# Distributed under the MIT License.
-# (See accompanying file LICENSE or copy at http://opensource.org/licenses/MIT)
-
-DST="lib/GL/glext.h"
-URL="https://www.opengl.org/registry/api/GL/glext.h"
-[[ ! -f $DST ]] && echo "Please run from root of repository" && exit 1
-curl $URL -o $DST
